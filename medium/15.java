b429a0bd882ee294ea600dac0607ba9ba1a9d4b6
Jaideep Gubbala <Jaideepdps13@gmail.com>
2026-06-11 00:26:06 +0530

    LeetCode #15

---
+++ medium/15.java
@@ -0,0 +1,14 @@
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                if (sum == 0) {
+                int sum = nums[i] + nums[left] + nums[right];
+            while (left < right) {
+            int left = i + 1, right = nums.length - 1;
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+        for (int i = 0; i < nums.length - 2; i++) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+    
+    public List<List<Integer>> threeSum(int[] nums) {
+class Solution {
