26afa140d83f7a303b519df12976f1540197033c
Jaideep Gubbala <Jaideepdps13@gmail.com>
2026-06-12 21:42:31 +0530

    LeetCode #8

---
+++ medium/8.java
@@ -0,0 +1,20 @@
+        }
+        while (i < s.length() && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+            }
+                sign = -1;
+            if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && s.charAt(i) - '0' > 7)) {
+                if (sign == -1) {
+        if (i < s.length() && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            if (s.charAt(i++) == '-') {
+        }
+        
+        while (i < s.length() && s.charAt(i) == ' ') {
+            i++;
+        
+        int res = 0, i = 0, sign = 1;
+        
+            return 0;
+        }
+        if (s.equals("")) {
+class Solution {
+    public int myAtoi(String s) {
