e9f48dadeacc1d22b3be0efb9b707f325ee4bae1
Jaideep Gubbala <Jaideepdps13@gmail.com>
2026-07-21 21:36:21 +0530

    LeetCode #12

---
+++ medium/12.java
@@ -0,0 +1,16 @@
+            ans += "V";
+            num -=5;
+        }
+        if(num>=5) {
+            num -=9;
+        }
+            ans += "IX";
+        }
+        if(num>=9) {
+            num -=10;
+            ans += "X";
+        while(num>=10) {
+        }
+            num -=40;
+            ans += "XL";
+        if(num>=40) {
