ee275343fffd18426f64d17c96f7508c949dc51f
Jaideep Gubbala <Jaideepdps13@gmail.com>
2026-08-03 10:20:44 +0530

    LeetCode #78

---
+++ medium/78.java
@@ -0,0 +1,13 @@
+        if (idx == nums.length) {
+            result.add(new ArrayList<>(presentList));
+            return;
+        }
+                              List<List<Integer>> result) {
+                              List<Integer> presentList,
+    public static void friend(int idx, int[] nums,
+        return res;
+    }
+        friend(0, nums, new ArrayList<>(), res);
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+class Solution {
